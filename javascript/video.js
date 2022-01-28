cc1950d0404fcf11570505e3311ab8044f7fb0df
Pfitto <98596002+lecelluleucariotiche@users.noreply.github.com>
2022-01-28 17:08:19 +0100

    changes

---
+++ javascript/video.js
@@ -0,0 +1,43 @@
+function toggle(){
+	var menu = document.getElementById('menu-content');
+	var b1 = document.getElementById('menu');
+	var coperto = document.getElementById('coperto');
+	coperto.classList.toggle('active')
+	b1.classList.toggle('active')
+	menu.classList.toggle('active')
+}
+
+function video1() {
+	var video1 = document.getElementById('video-1-div');
+	var coperto = document.getElementById('coperto2');
+	coperto.classList.toggle('active')
+	video1.classList.toggle('active')
+}
+
+function video2() {
+	var video2 = document.getElementById('video-2-div');
+	var coperto = document.getElementById('coperto3');
+	coperto.classList.toggle('active')
+	video2.classList.toggle('active')
+}
+
+function video3() {
+	var video2 = document.getElementById('video-3-div');
+	var coperto = document.getElementById('coperto4');
+	coperto.classList.toggle('active')
+	video2.classList.toggle('active')
+}
+
+function video4() {
+	var video2 = document.getElementById('video-4-div');
+	var coperto = document.getElementById('coperto5');
+	coperto.classList.toggle('active')
+	video2.classList.toggle('active')
+}
+
+function video5() {
+	var video2 = document.getElementById('video-5-div');
+	var coperto = document.getElementById('coperto6');
+	coperto.classList.toggle('active')
+	video2.classList.toggle('active')
+}
